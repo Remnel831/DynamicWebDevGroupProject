5b7c8376771e01429d0cc25fbe598e590a070026
Joseph Gutierrez <smallninja.jay@gmail.com>
2024-06-19 15:55:41 -0500

    deleted test file and and added test db file

---
+++ databse.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+$username = "test_user";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+?>
