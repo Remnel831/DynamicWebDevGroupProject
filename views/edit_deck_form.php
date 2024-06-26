7e42c9ee317245e7f222597ad23ee7a891c25c63
Mario Montes <mdmontes94@gmail.com>
2024-06-25 19:56:50 -0500

    renamed file, added deleteCard file, code to follow, want to make sure things are sound before adding it into MVC format

--- views/edit_deck_form.php
+++ views/edit_deck_form.php
@@ -1,3 +1,8 @@
+<? php
+require_once(__DIR__ . '/../config/database.php');
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,8 +11,13 @@
         <title>Edit Deck</title>
     </head>
     <body>
+    <nav class="navMenu">
+        <a href="index.php">Main Page</a>
+        <a href="index.php?action=all_decks"><b>Deck List</b></a>
+        <a href="index.php?action=all_cards">Card List</a>
+    </nav>
         <form action='editDeck.php' method="post">
-            
+
         </form>
     </body>
 </html>
