c30ba8b448dad48bd07f9f5a102a44b0e4d1cdc7
Mario Montes <mdmontes94@gmail.com>
2024-06-25 19:18:25 -0500

    simple edit deck php file, with plans to integrate it into the best model

--- views/edit_deck_form.php
+++ views/edit_deck_form.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Deck</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Edit Deck</title>
+    </head>
+    <body>
+        <form action='editDeck.php' method="post">
+            
+        </form>
+    </body>
 </html>
