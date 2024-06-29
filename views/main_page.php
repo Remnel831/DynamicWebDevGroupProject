f65fb7e36a322dfe294531393935b313c08920ae
Mario Montes <mdmontes94@gmail.com>
2024-06-29 08:54:51 -0500

    Started work with the "Magic Mini Lesson"

--- views/main_page.php
+++ views/main_page.php
@@ -1,3 +1,31 @@
+<?php
+
+require_once(__DIR__ . '/../config/database.php');
+
+class TypesManager {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = getConnection(); // Assumes getConnection() function from database.php
+    }
+
+    public function getCardTypes() {
+        $queryTypes = 'SELECT *
+                       FROM mtg_card_type';
+        $statement = $this->conn->prepare($queryTypes);
+        $statement->execute();
+        $types = $statement->fetchAll();
+        $statement->closeCursor();
+
+        return $types;
+    }
+}
+
+$typeManager = new TypesManager();
+$types = $typeManager->getCardTypes();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -19,6 +47,23 @@
                 to get an overview of what each card type does, see what available sets/cards you can build decks from, 
                 as well as allowing you to build your own decks!
             </h2>
+            <div class="sub-container">
+                <h1>The Magic Mini Lesson</h1>
+                <h2>Week of 06/30/24</h2>
+                <p>This week we will cover the five basic mana colors and the card types.</p>
+                <table class="card-types">
+                    <tr>
+                        <th>Card Type</th>
+                        <th>Description</th>
+                    </tr>
+                    <?php foreach($types as $type) : ?>
+                    <tr>
+                        <td><?php echo $type['type_name'] ?></td>
+                        <td><?php echo $type['type_description'] ?></td>
+                    </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
         </div>
     </body>
 </html>
