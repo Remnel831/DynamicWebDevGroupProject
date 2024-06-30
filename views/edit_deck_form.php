28d05e1c5f7c98f8477f4be3526e87ce1001d399
Mario Montes <mdmontes94@gmail.com>
2024-06-30 17:02:33 -0500

    Added comments to the edit deck page

--- views/edit_deck_form.php
+++ views/edit_deck_form.php
@@ -1,3 +1,9 @@
+<!--
+Author(s): Mario Montes
+Description: This page allows the user to edit their deck list by adding/deleting cards from their list or updating the 
+card count for the current deck that is being edited.
+-->
+
 <?php
 require_once(__DIR__ . '/../config/database.php');
 
@@ -8,6 +14,13 @@ class DeckManager {
         $this->conn = getConnection(); // Assumes getConnection() function from database.php
     }
 
+    /*
+    The following function gets the card list of the deck that is needing to be edited, by utilizing the get method.
+    Then, once the deck_id is grabbed, it performs an SQL query that grabs the card list where the deck_id matches
+    the deck_id in the decks_cards table. It also uses a join from the main cards table, in order to get the names for the
+    cards which would be what the user would see. -MM
+    */
+    
     public function getCardList() {
         if (isset($_GET['deck_id'])) {
             $deck_id = $_GET['deck_id'];
@@ -25,6 +38,11 @@ class DeckManager {
         return $cardList;
     }
 
+    /*
+    The following function grabs all of the cards table and uses a join with the card_sets table so that the 
+    card set appears when selecting a card to add to the deck. - MM
+    */
+
     public function getAllCards() {
         $queryAllCards = 'SELECT card.*, sets.set_name 
                           FROM mtg_cards card 
@@ -38,6 +56,11 @@ class DeckManager {
     }
 }
 
+
+/*
+The following section of code assigns the php values of $deck_id, $cardList, and $allCards to their corresponding functions
+in order to sucessfully have the arguments needed to add/edit/update the current deck.
+*/
 $deck_id = filter_input(INPUT_GET, 'deck_id', FILTER_VALIDATE_INT);
 
 $deckManager = new DeckManager();
@@ -61,6 +84,13 @@ $allCards = $deckManager->getAllCards();
     </nav>
     <div class="container">
         <h3>Delete Card from Deck/Update Number of a Card</h3>
+        
+        <!--
+        The following form either deletes the selected card or updates the card count of the selected card
+        of the current user deck. It utilizes a foreach loop to pull up the card list for the current deck in the first
+        select input, and the second select input has four simple options to adjust the card count (as most 
+        formats of MTG allow only up to 4 of any card that isn't a basic land). 
+        -->
         <form class="editForm" method="post">
             <input type="hidden" name="deck_id" value="<?php echo $deck_id; ?>" />
             <label>Card: </label>
@@ -85,6 +115,14 @@ $allCards = $deckManager->getAllCards();
         </form>
         <br><br>
         <h3>Add Card to Deck</h3>
+        <!--
+        The following form adds a card to the deck list of the current deck by having the user first select a color,
+        then the list is brought up that has the corresponding color as the first result if a combination of colors are
+        applicable to the card. The first select input, named "color_filter" performs the javascript function during the onchange
+        which affects which cards in the list of cards to add in the second select input. The second select input uses a php
+        foreach loop function in order to populate the options with the appropriate cards.
+        -->
+        
         <form class="editForm" method="post">
             <input type="hidden" name="deck_id" value="<?php echo $deck_id; ?>" />
             <label>Color: </label>
@@ -112,16 +150,22 @@ $allCards = $deckManager->getAllCards();
         <footer name="deck_id" id="deck_id">Deck ID: <?php echo $deck_id; ?></footer>
     </div>
     <script>
+    /*
+    The following JS function filters the card results based on the main color of the card.
+    It uses the getElementById function to get the selected option from the corresponding
+    color select, then adjusted the card list select input based on the onchange of the color_filter
+    select. It also changes the first option on the card to add select input to the first option
+    of the selected color.
+    */
+    
     function filterCardsByColor() {
         var selectedColor = document.getElementById('color_filter').value;
-        console.log("Selected Color: ", selectedColor); // Debugging line
         var cardOptions = document.getElementById('card_id_add').options;
         var firstVisibleOptionIndex = -1;
 
         for (var i = 0; i < cardOptions.length; i++) {
             var option = cardOptions[i];
             var optionColors = option.getAttribute('data-color').toLowerCase().split('/');
-            console.log("Option Colors: ", optionColors); // Debugging line
 
             if (selectedColor === 'all' || optionColors[0] === selectedColor) {
                 option.style.display = '';
